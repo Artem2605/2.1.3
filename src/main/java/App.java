75a7614721c5e0cc3938c5bd3d5c4c0e67de1d22
Artem2605 <artyom260599@mail.ru>
2023-11-07 03:03:40 +0300

    forKata

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld firstHelloBean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld secondHelloBean = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(firstHelloBean == secondHelloBean);
+        Cat firstCatBean = applicationContext.getBean("cat", Cat.class);
+        Cat secondCatBean = applicationContext.getBean("cat", Cat.class);
+        System.out.println(firstCatBean == secondCatBean);
     }
 }
